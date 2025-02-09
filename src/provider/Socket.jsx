da923e93e6c7093dce1837826688c96076a1684c
Rishabh7778 <saty665566@gmai.com>
2025-02-09 16:40:41 +0530

    second commit

--- src/provider/Socket.jsx
+++ src/provider/Socket.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("http://localhost:8001")); 
+  const socket = useMemo(() => io("https://videochat-server-vtap.onrender.com")); 
 
   return (
     <SocketContext.Provider value={{ socket }}>
